b50f19ba9f4aa9242d0bfd6bd7ab5d44739e06d8
Yash Shrivastava <yshrivastava@deloitte.com>
2021-11-15 23:00:31 +0530

    Added DB Connection method 2

--- server/connection/db.js
+++ server/connection/db.js
@@ -1,19 +1,24 @@
 import mongoose from 'mongoose'
 
-const Connection = async () => {
-    try{
-        const connectionString=`mongodb://dbUser:passfordb@content-aggregator-shard-00-00.ajhhe.mongodb.net:27017,content-aggregator-shard-00-01.ajhhe.mongodb.net:27017,content-aggregator-shard-00-02.ajhhe.mongodb.net:27017/content-aggregator?ssl=true&replicaSet=atlas-lqnuzj-shard-0&authSource=admin&retryWrites=true&w=majority`
+//Async DB Connection method
 
-        await mongoose.connect(connectionString,{useNewUrlParser : true});
-        console.log(`DB connected!`)
-    }
-    catch(err){
-        console.log(`Oops! DB connection err`,err)
-    }
+// const Connection = async () => {
+//     try{
+//         const connectionString=`mongodb://dbUser:passfordb@content-aggregator-shard-00-00.ajhhe.mongodb.net:27017,content-aggregator-shard-00-01.ajhhe.mongodb.net:27017,content-aggregator-shard-00-02.ajhhe.mongodb.net:27017/content-aggregator?ssl=true&replicaSet=atlas-lqnuzj-shard-0&authSource=admin&retryWrites=true&w=majority`
 
-} 
-    // mongoose.connect(`mongodb://dbUser:passfordb@content-aggregator-shard-00-00.ajhhe.mongodb.net:27017,content-aggregator-shard-00-01.ajhhe.mongodb.net:27017,content-aggregator-shard-00-02.ajhhe.mongodb.net:27017/content-aggregator?ssl=true&replicaSet=atlas-lqnuzj-shard-0&authSource=admin&retryWrites=true&w=majority`)
-    // .then(()=>console.log(`DB connected!`))
-    // .catch(err=>console.log(`Oops! DB connection err: ${err}`))
+//         await mongoose.connect(connectionString,{useNewUrlParser : true});
+//         console.log(`DB connected!`)
+//     }
+//     catch(err){
+//         console.log(`Oops! DB connection err`,err)
+//     }
 
+// }
+
+const Connection=()=>{ 
+    
+    mongoose.connect(`mongodb://dbUser:passfordb@content-aggregator-shard-00-00.ajhhe.mongodb.net:27017,content-aggregator-shard-00-01.ajhhe.mongodb.net:27017,content-aggregator-shard-00-02.ajhhe.mongodb.net:27017/content-aggregator?ssl=true&replicaSet=atlas-lqnuzj-shard-0&authSource=admin&retryWrites=true&w=majority`)
+    .then(()=>console.log(`DB connected!`))
+    .catch(err=>console.log(`Oops! DB connection err: ${err}`))
+}
 export default Connection;
