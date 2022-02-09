41720ab606a73dba301202ab73588c56774bdf51
Yash Shrivastava <yshrivastava@deloitte.com>
2022-02-10 01:08:33 +0530

    setting up the base

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 import express from 'express'; // lib import
+import cors from 'cros'
+import bodyParser from 'body-parser'
+
 
 import Connection from './connection/db.js' // component import 
+import Route from './routes/route.js'
 
 const app = express();
 
+app.use('/', cors());
+app.use('/', bodyParser.urlencoded({extended : true})); // body parser for urlencoded and json content types 
+app.use('/', bodyParser.json({extended : true}))
+app.use('/', Route)
 
 
 Connection();
 
-app.listen(8000, ()=>console.log(`Server running on port 8000`));
+app.listen(8000, ()=>console.log(`Sever running on port 8000`));
