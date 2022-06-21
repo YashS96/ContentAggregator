d9834ba0f8cbdff60cdd5fd9968d6fcda34736d8
Yash Shrivastava <srivastavyash7@gmail.com>
2022-06-21 19:10:33 +0530

    cluster module added"

---
+++ server/test/testing-url.js
@@ -0,0 +1,14 @@
+/*
+// For testing back-end API's in your app:
+// use url where back-ebd server is hosted
+// const TESTING_URL =``
+// for local testing:
+const TESTING_HOST ='localhost' 
+const TESTING_PORT = 8000
+const TESTING_URL = `http://${TESTING_HOST}:${TESTING_PORT}`
+*/
+
+// External APIs:
+export const BBC = 'http://feeds.bbci.co.uk/news/rss.xml'
+export const NDTV = 'http://feeds.feedburner.com/ndtvnews-top-stories?format=xml'
+export const HINDU = 'https://www.thehindu.com/news/international/feeder/default.rss'
